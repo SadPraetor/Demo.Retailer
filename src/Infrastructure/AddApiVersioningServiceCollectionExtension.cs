acc930cf605a9738b95d38bbf88040692651c8a5
SadPraetor <SadPraetor@gmail.com>
2025-02-07 22:57:34 +0100

    refactor, moving some api version and open api registration to extension methods

---
+++ src/Infrastructure/AddApiVersioningServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Infrastructure
+{
+	public static class AddApiVersioningServiceCollectionExtension
+	{
+		public static IServiceCollection AddProjectApiVersions(this IServiceCollection services)
+		{
+			services.AddApiVersioning(options =>
+			{
+				options.DefaultApiVersion = new ApiVersion(1);
+				options.ReportApiVersions = true;
+				options.AssumeDefaultVersionWhenUnspecified = true;
+				options.ApiVersionReader = new UrlSegmentApiVersionReader();
+			})
+			.AddApiExplorer(o =>
+			{
+				o.GroupNameFormat = "'v'VVV";
+				o.SubstituteApiVersionInUrl = true;
+			});
+			return services;
+		}
+	}
+}
